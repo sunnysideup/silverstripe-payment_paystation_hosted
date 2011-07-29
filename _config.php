aa2f8fab4255ea32637b79b4a8bba7bec27062e2
nfrancken@gmail.com <nfrancken@gmail.com@950b781c-c544-efab-2f99-90083ee69683>
2011-07-29 07:30:49 +0000

    moving around some payment gateways
    git-svn-id: https://silverstripe-ecommerce.googlecode.com/svn/modules/payment_paystation_hosted/trunk@1387 950b781c-c544-efab-2f99-90083ee69683

---
+++ _config.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ *
+ *
+ * 
+**/
+
+
+//copy the lines between the START AND END line to your /mysite/_config.php file and choose the right settings
+//===================---------------- START payment_paystation_hosted MODULE ----------------===================
+//===================---------------- END payment_paystation_hosted MODULE ----------------===================

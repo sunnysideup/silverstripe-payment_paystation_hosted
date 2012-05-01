e28216cd0c21aa4c7fb50681ed7045523a528c87
nfrancken@gmail.com <nfrancken@gmail.com@950b781c-c544-efab-2f99-90083ee69683>
2012-05-01 09:57:48 +0000

    MINOR
    git-svn-id: https://silverstripe-ecommerce.googlecode.com/svn/modules/payment_paystation_hosted/trunk@2570 950b781c-c544-efab-2f99-90083ee69683

--- code/ImprovedPaystationHostedPayment.php
+++ code/ImprovedPaystationHostedPayment.php
@@ -3,22 +3,22 @@
 /**
  * PaystationHostedPayment - www.paystation.co.nz
  * Contains improvements
- * 
+ *
  * Test cards:
  * Type - number - expiry - security code
  * VISA - 5123456789012346 - 0513 - 100
  * MASTERCARD - 5123456789012346 - 0513 - 100
- * 
+ *
  * How to get different responses (by changing transaction cents value):
  * cents - response - response code
  * .00 - approved - 0
- * .51 - Insufficient Funds -5 
+ * .51 - Insufficient Funds -5
  * .57 - Invalid transaction - 8
  * .54 - Expired card - 4
  * .91 - Error communicating with bank - 6
- * 
+ *
  * URL paramaters:
- * 
+ *
  * paystation (REQUIRED)
  * pstn_pi = paystation ID (REQUIRED) - This is an initiator flag for the payment engine and can be nothing, or if your environment requires to assign a value please send ‘_empty’
  * pstn_gi = Gateway ID (REQUIRED) - The Gateway ID that the payments will be made against
@@ -29,17 +29,17 @@
  * pstn_mr = Merchant Reference Code - a non-unique reference code which is stored against the transaction. This is recommended because it can be used to tie the transaction to a merchants customers account, or to tie groups of transactions to a particular ledger at the merchant. This will be seen from Paystation Admin. pstn_mr can be empty or omitted.
  * pstn_ct = Card Type - the type of card used. When used, the card selection screen is skipped and the first screen displayed from the bank systems is the card details entry screen. Your merchant account must be enabled for External Payment Selection (EPS), you may have to ask your bank to enable this - check with us if you have problems. CT cannot be empty, but may be omitted.
  * pstn_af = Ammount Format - Tells Paystation what format the Amount is in. If omitted, it will be assumed the amount is in cents
- * 
+ *
 */
 
 class ImprovedPaystationHostedPayment extends Payment {
-	
+
 	static $db = array(
 		'MerchantSession' => 'Varchar',
 		'TransactionID' => 'Varchar'
 	);
-	
-	
+
+
 	protected static $privacy_link = 'http://paystation.co.nz/privacy-policy';
 	protected static $logo = 'payment/images/payments/paystation.jpg';
 	protected static $url = 'https://www.paystation.co.nz/direct/paystation.dll';
@@ -47,15 +47,15 @@ class ImprovedPaystationHostedPayment extends Payment {
 	protected static $paystation_id;
 	protected static $gateway_id;
 	protected static $merchant_ref;
-	
+
 	protected static $returnurl = null;
-	
+
 	//setters
-	
+
 	static function set_test_mode() {
 		self::$test_mode = true;
 	}
-	
+
 	static function set_return_url($url){
 		self::$returnurl = $url;
 	}
@@ -67,15 +67,15 @@ class ImprovedPaystationHostedPayment extends Payment {
 	static function set_gateway_id($gateway_id) {
 		self::$gateway_id = $gateway_id;
 	}
-	
+
 	static function set_merchant_ref($merchant_ref) {
 		self::$merchant_ref = $merchant_ref;
 	}
-	
+
 	static function get_paystation_id(){
 		return self::$paystation_id;
 	}
-	
+
 	function getPaymentFormFields() {
 		$logo = '<img src="' . self::$logo . '" title="Credit card payments powered by Paystation"/>';
 		$privacyLink = '<a href="' . self::$privacy_link . '" target="_blank" title="Read Paystation\'s privacy policy">' . $logo . '</a><br/>';
@@ -84,77 +84,80 @@ class ImprovedPaystationHostedPayment extends Payment {
 			new LiteralField(
 				'PaystationPaymentsList',
 				'<img src="payment/images/payments/methods/visa.jpg" title="Visa"/>' .
-				'<img src="payment/images/payments/methods/mastercard.jpg" title="MasterCard"/>' 
+				'<img src="payment/images/payments/methods/mastercard.jpg" title="MasterCard"/>'
 			)
 		);
 	}
-	
+
 	function getPaymentFormRequirements() {
 		return null;
 	}
-	
+
 	function processPayment($data, $form) {
-		
+
 		//check for correct set up info
 		if(!self::$paystation_id)
 			user_error('No paystation id specified. Use PaystationHostedPayment::set_paystation_id() in your _config.php file.', E_USER_ERROR);
 		if(!self::$gateway_id)
 			user_error('No gateway id specified. Use PaystationHostedPayment::set_gateway_id() in your _config.php file.', E_USER_ERROR);
-		
+
 		//merchant Session: built from (php session id)-(payment id) to ensure uniqueness
 		$this->MerchantSession = session_id()."-".$this->ID;
-		
+
 		//set up required parameters
 		$data = array(
 			'paystation' => '_empty',
 			'pstn_pi' => self::$paystation_id, //paystation ID
 			'pstn_gi' => self::$gateway_id, //gateway ID
-			'pstn_ms' => $this->MerchantSession, 
+			'pstn_ms' => $this->MerchantSession,
 			'pstn_am' => $this->Amount * 100 //ammount in cents
 		);
-		
+
 		//add optional parameters
 		//$data['pstn_cu'] = //currency
 		if(self::$test_mode) $data['pstn_tm'] = 't'; //test mode
 		if(self::$merchant_ref) $data['pstn_mr'] = self::$merchant_ref; //merchant refernece
 		//$data['pstn_ct'] = //card type
 		//$data['pstn_af'] = //ammount format
-		
+
 		//Make POST request to Paystation via RESTful service
 		$paystation = new RestfulService(self::$url,0); //REST connection that will expire immediately
 		$paystation->httpHeader('Accept: application/xml');
 		$paystation->httpHeader('Content-Type: application/x-www-form-urlencoded');
-		
+
 		$data = http_build_query($data);
-		$response = $paystation->request('','POST',$data);		
+		$response = $paystation->request('','POST',$data);
 		$sxml = $response->simpleXML();
-	
+
 		//set up a page for redirection
 		$page = new Page();
 		$page->Logo = '<img src="'.self::$logo.'" alt="Payments powered by Paystation"/>';
 		$controller = new Page_Controller($page);
-		
+
 		if($paymenturl = $sxml->DigitalOrder){
 			//TODO: store order details
-			
+
 			$page->Title = 'Redirection to Paystation...';
 			$page->Loading = true;
 			$page->Message = "redirecting to paystation payment";
 			$page->ExtraMeta = '<meta http-equiv="Refresh" content="1;url='.$paymenturl.'"/>';
-			
+
 			$this->Status = 'Incomplete';
 			$this->write();
-			
-			Requirements::javascript(THIRDPARTY_DIR . '/jquery/jquery.js'); //javascript redirection
+
+			Requirements::javascript(THIRDPARTY_DIR."/jquery/jquery.js");
+			//Requirements::block(THIRDPARTY_DIR."/jquery/jquery.js");
+			//Requirements::javascript(Director::protocol()."ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js");
+
 			$output = $controller->renderWith('PaymentProcessingPage');
-			
+
 			Director::redirect($paymenturl); //redirect to payment gateway
 			return new Payment_Processing($output);
 		}
-		
-		
+
+
 		if(isset($sxml->PaystationErrorCode) && $sxml->PaystationErrorCode > 0){
-			
+
 			//provide useful feedback on failure
 			$error = $sxml->PaystationErrorCode." ".$sxml->PaystationErrorMessage;
 
@@ -164,13 +167,13 @@ class ImprovedPaystationHostedPayment extends Payment {
 			//user_error('Paystation error: $error');
 			return new Payment_Failure($sxml->PaystationErrorMessage);
 		}
-		
+
 		//else recieved bad xml or transaction falied for an unknown reason
 		//what should happen here?
-		
+
 		return new Payment_Failure("Unknown error");
 	}
-	
+
 	function ProcessError($errorcode){
 		//if errorcode = 4,5,7?
 		//then user has failed in some way
@@ -189,13 +192,13 @@ class ImprovedPaystationHostedPayment extends Payment {
 			</script>
 HTML;
 	}
-	
+
 	function redirectToReturnURL(){
 		if(self::$returnurl){
 			Director::redirect(self::$returnurl.'/'.$this->ID);
 			return;
 		}
-		//TODO: show some default thing if there's no return url?...or throw error immediately in the processPayment method?		
+		//TODO: show some default thing if there's no return url?...or throw error immediately in the processPayment method?
 	}
 }
 
@@ -203,16 +206,16 @@ HTML;
  * Handler for responses from the PayPal site
  */
 class ImprovedPaystationHostedPayment_Handler extends Controller {
-	
+
 	protected static $usequicklookup = true;
 	protected static $quicklookupurl = 'https://www.paystation.co.nz/lookup/quick/';
 
 	static $URLSegment = 'paystation';
-	
+
 	static function complete_link() {
 		return self::$URLSegment . '/complete';
 	}
-	
+
 	function complete() {
 		//TODO: check that request came from paystation.co.nz
 
@@ -224,7 +227,7 @@ class ImprovedPaystationHostedPayment_Handler extends Controller {
 					if($_REQUEST['ti']) $payment->TransactionID = $_REQUEST['ti'];
 					if($_REQUEST['em']) $payment->Message = $_REQUEST['em'];
 					$this->Status = 'Success';
-					
+
 					//Quick Lookup
 					if(self::$usequicklookup){
 						$paystation = new RestfulService(self::$quicklookupurl,0); //REST connection that will expire immediately
@@ -237,31 +240,31 @@ class ImprovedPaystationHostedPayment_Handler extends Controller {
 						);
 						$paystation->setQueryString($data);
 						$response = $paystation->request(null,'GET');
-						
+
 						$sxml = $response->simpleXML();
 						echo "<br/>";
-						if($sxml && $s = $sxml->LookupResponse){					
-							
+						if($sxml && $s = $sxml->LookupResponse){
+
 							//check transaction ID matches
 							if($payment->TransactionID != (string)$s->PaystationTransactionID){
 								$payment->Status = "Failure";
 								$payment->Message .= "The transaction ID didn't match.";
 							}
-							
+
 							//check amount matches
 							if($payment->Amount*100 != (int)$s->PurchaseAmount){
 								$payment->Status = "Failure";
 								$payment->Message .= "The purchase amount was inconsistent.";
 							}
-							
+
 							//check session ID matches
 							if(session_id() != substr($_REQUEST['ms'],0,strpos($_REQUEST['ms'],'-'))){
 								$payment->Status = "Failure";
 								$payment->Message .= "Session id didn't match.";
 							}
-							
+
 							//TODO: extra - check IP address against $payment->IP??
-							
+
 						}elseif($sxml && $s = $sxml->LookupStatus){
 							$payment->Status = "Failure";
 							$payment->Message .= $s->LookupMessage;
@@ -270,7 +273,7 @@ class ImprovedPaystationHostedPayment_Handler extends Controller {
 							$payment->Status = "Failure";
 							$payment->Message .= "Paystation quick lookup failed.";
 						}
-	
+
 					}
 					$payment->write();
 					$payment->redirectToReturnURL();
@@ -281,12 +284,12 @@ class ImprovedPaystationHostedPayment_Handler extends Controller {
 			else user_error('There is no any Paystation hosted payment ID specified', E_USER_ERROR);
 		}
 		else user_error('There is no any Paystation hosted payment error code specified', E_USER_ERROR);
-		
+
 		//TODO: sawp errors for payment failures??
 	}
-	
+
 	/** Quick Lookup Refernce:
- 	 * 
+ 	 *
 	 * AcquirerName - Merchants Acquirer Name
 	 * AcquirerMerchantID - The acquirer’s merchant ID used for the transaction
 	 * PaystationUserID - Paystation username
@@ -301,8 +304,8 @@ class ImprovedPaystationHostedPayment_Handler extends Controller {
 	 * BatchNumber - The Batch number on the Payment Server that this transaction will be added to in order to be processed by the acquiring institution.
 	 * Cardtype - The card type used
 	 */
-	
-	
+
+
 }
 
 ?>
